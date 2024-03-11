fe29dbf388ce47e2bb8513991fae927b57b725c4
my name <blueshaperd3@gmail.com>
2024-03-12 00:58:01 +0500

    Arrays

---
+++ app.js
@@ -0,0 +1,27 @@
+// var students = ["Rehan", "Ali", "Imran", "Bilal", "Rizwan"];
+// alert("Welcome to class " + students [3]);
+// console.log(students);
+
+
+// pop delet from ends
+// var students = ["Rehan", "Ali", "Imran", "Bilal", "Rizwan"];
+// students.pop()
+// console.log(students);
+
+
+// push add from ends
+// var students = ["Rehan", "Ali", "Imran", "Bilal", "Rizwan"];
+// students.push("Nabeel")
+// console.log(students);
+
+
+// shift delet from starts
+// var students = ["Rehan", "Ali", "Imran", "Bilal", "Rizwan"];
+// students.shift()
+// console.log(students);
+
+
+// unshift add from
+var students = ["Rehan", "Ali", "Imran", "Bilal", "Rizwan"];
+ students.push("Nabeel")
+ console.log(students);
